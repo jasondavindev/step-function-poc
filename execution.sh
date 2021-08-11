5f0ac9d23e993473c424bcc6ce10e001ffe2bf32
jason.carneiro <jason.carneiro@willbank.com.br>
2021-08-11 11:23:09 -0300

    start poc

---
+++ execution.sh
@@ -0,0 +1,18 @@
+aws stepfunctions \
+  --endpoint http://localhost:8083 \
+  create-state-machine \
+  --definition file://functions/simple.json \
+  --name "SimpleStateMachine" \
+  --role-arn "arn:aws:iam::012345678901:role/DummyRole"
+
+aws stepfunctions \
+  --endpoint http://localhost:8083 \
+  start-execution \
+  --state-machine arn:aws:states:us-east-1:123456789012:stateMachine:SimpleStateMachine \
+  --name test3 \
+  --input '{"IsHelloWorldExample":true}'
+
+aws stepfunctions \
+  --endpoint http://localhost:8083 \
+  describe-execution \
+  --execution-arn arn:aws:states:us-east-1:123456789012:execution:SimpleStateMachine:test2
